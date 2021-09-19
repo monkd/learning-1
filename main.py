bb3e9085fd4e9481c9646a63797725d6c71cf416
monkd <60039902+monkd@users.noreply.github.com>
2021-09-19 16:10:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-
 import sys
 import re
+
+keycnt=0
 
 KEYword = (
     "unsigned", "void", "volatile", "while", "struct", "switch", "typedef", "union",
@@ -12,14 +13,26 @@ KEYword = (
 
 def simplity(path):
     text = open(path, mode='r').read()
-    textre = re.sub(r"[0123456789]+", "  ", text)
-    textre = re.sub(r"[\n]+", "  ", textre)
-    textre = re.sub(r"['+*/=<>()''-']+", "  ", textre)
-    textre = re.sub(r"[ \f\r\t\v]+", " ", textre)
-    print(textre)
+    textre = re.sub(r"[0123456789]+", "  ", text)   #将代码中的数字都变成空格，因为数字对我们的统计没影响
+    textre = re.sub(r"[\n]+", "  ", textre)         #将代码中的换行都变成空格
+    textre = re.sub(r"['+*/=<>()''-']+", "  ", textre)         #将代码中的运算符都变成空格
+    textre = re.sub(r"[ \f\r\t\v]+", " ", textre)   #将代码中的多余空格删除
+    #print(textre)
+    textre = re.split(r"\W", textre)                #转化为列表
     return textre
 
+def cntfuc(textre):
+    global keycnt
+    text_iter = iter(range(len(textre)))
+    for i in text_iter:
+        temp = textre[i]
+        if temp != ' 'and temp in KEYword:
+            keycnt +=1
+                
+            
+    print("total num: ",keycnt);
 
 if __name__ == "__main__":
     PATH = sys.argv[1]
     FILE = simplity(PATH)
+    cntfuc(FILE)
